37ac49fa69ef91c095425b86c4c8293e6100772d
Dragonknight2467 <73117071+DraconicKnights@users.noreply.github.com>
2023-08-15 16:09:39 +0100

    Interaction Update

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -26,35 +26,41 @@ module.exports = {
                 break;
 
             case InteractionType.ModalSubmit:
-                if (interaction.customId === 'reportmodal') {
-                    const username = interaction.fields.getTextInputValue('usernamereport');
-                    const reason = interaction.fields.getTextInputValue('reasonreport');
-
-                    console.log(interaction.user.username + " Has submited a modal ID: " + interaction.customId);
-
-
-                    const embed = new EmbedBuilder()
-                    .setTitle('Member Report')
-                    .setImage(interaction.user.displayAvatarURL())
-                    .setColor('Blue')
-                    .setDescription(`${interaction.user.username} has made a report`)
-                    .addFields(
-                        { name: 'Username', value: `${username}`, inline: true },
-                        { name: 'Reason', value: `${reason}`, inline: true },
-                    )
-
-                    const channelid = '';
-
-                    const channel = interaction.guild.channels.cache.get(channelid);
-
-                    channel.send({embeds: [embed]})
-
-                    interaction.reply({content: 'Report has been submited', ephemeral: true});
-
-
-                }
+                ModalFunc(interaction.customId);
+                break;
         }
 
 
+    }
+}
+
+
+function ModalFunc(modalId) {
+    switch (modalId) {
+        case 'reportmodal':
+            const username = interaction.fields.getTextInputValue('usernamereport');
+            const reason = interaction.fields.getTextInputValue('reasonreport');
+
+            console.log(interaction.user.username + " Has submited a modal ID: " + interaction.customId);
+
+
+            const embed = new EmbedBuilder()
+            .setTitle('Member Report')
+            .setImage(interaction.user.displayAvatarURL())
+            .setColor('Blue')
+            .setDescription(`${interaction.user.username} has made a report`)
+            .addFields(
+                { name: 'Username', value: `${username}`, inline: true },
+                { name: 'Reason', value: `${reason}`, inline: true },
+            )
+
+            const channelid = '';
+
+            const channel = interaction.guild.channels.cache.get(channelid);
+
+            channel.send({embeds: [embed]})
+
+            interaction.reply({content: 'Report has been submited', ephemeral: true});
+            break;
     }
 }
